38817111ececfafe859846356483d28cd1dcd178
Madhurima Chakraborty <56744323+MadhuNimmo@users.noreply.github.com>
2022-12-14 11:08:40 -0800

    Update babel.config.js

--- preset-env/babel.config.js
+++ preset-env/babel.config.js
@@ -1,1 +1,13 @@
-
+module.exports = function (api) {
+  api.cache(false);
+  return {
+    presets: ["module:metro-react-native-babel-preset", ['@babel/preset-env',
+      {
+        "loose": true,
+        "useBuiltIns": "entry",
+        "corejs": "2.6.8",
+        "modules": false,
+      }]],
+    plugins: ['remove-use-strict']
+  };
+};
